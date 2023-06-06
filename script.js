c36dad7c7cfeca384bffa387721300ee12117c9f
Your Nam <you@example.com>
2023-06-06 09:19:37 +0200

    mukodokepes

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function kiir(){
     fetch("https://dog.ceo/api/breeds/image/random")
     .then(adat=>adat.json())
     .then(res => {
-        document.getElementById("uzenet").innerHTML = res("messege")
+        document.getElementById("kep").src = res["message"]
     })
 }
